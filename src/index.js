45c36e1a6fbc0d46e1f508ad3f22668fb140a66c
Manuel Rodríguez Santana <lolo@turitop.com>
2023-03-10 21:01:38 +0000

    Add code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-const a = process.env.NODE_ENV;
-console.log(a)
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('mode').innerHTML = process.env.NODE_ENV;
+  document.getElementById('version').innerHTML = process.env.MY_APP_VERSION;
+  console.log(process.env);
+})
