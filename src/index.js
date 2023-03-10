29324d069c5a6fea11072879fe3ca599f2bb9d52
Manuel Rodríguez Santana <lolo@turitop.com>
2023-03-10 21:03:43 +0000

    Remove console.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   document.getElementById('mode').innerHTML = process.env.NODE_ENV;
   document.getElementById('version').innerHTML = process.env.MY_APP_VERSION;
-  console.log(process.env);
 })
