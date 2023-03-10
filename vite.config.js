b5a4e38a0a2927ab9b304af380b9ab1a9bbaea9a
Manuel Rodríguez Santana <lolo@turitop.com>
2023-03-10 20:47:31 +0000

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ const isNetlify = process.env.NETLIFY || false;
 const folderName = path.basename(process.cwd()) + "/";
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
-const noMinify = isNetlify && mode !== "production"
-const minify = noMinify ? { minify: false, minifySyntax: false } : {};
-
 export default {
   root: "src",
   base,
@@ -14,7 +11,6 @@ export default {
   publicDir: "../public",
   build: {
     outDir: "../dist",
-    assetsDir: "./",
-    minify,
+    assetsDir: "./"
   }
 };
