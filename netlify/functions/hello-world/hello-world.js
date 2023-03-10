c0c33c039ad17d1fc081fb4bc13eba227349d346
Manuel Rodríguez Santana <lolo@turitop.com>
2023-03-10 22:58:01 +0000

    add function

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,7 +1,7 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const subject = event.queryStringParameters.name || 'World!'
     return {
       statusCode: 200,
       body: JSON.stringify({ message: process.env.MY_APP_VERSION }),
